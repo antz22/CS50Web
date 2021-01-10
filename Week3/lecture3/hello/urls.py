99e461d51f12b661b8c81268b9195d520ef21a41
bobycat22 <anthonyznj@gmail.com>
2021-01-10 00:42:30 -0500

    Add lecture 3 notes

---
+++ Week3/lecture3/hello/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
+    path("anthony", views.anthony, name="anthony"),
+    path("david", views.david, name="david")
+]
