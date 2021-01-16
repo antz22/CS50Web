2edaf40747f4e12645043e9bb2cfc27744161045
bobycat22 <anthonyznj@gmail.com>
2021-01-16 00:05:10 -0500

    Project 1 Update

--- Week3/Project1/wiki/encyclopedia/urls.py
+++ Week3/Project1/wiki/encyclopedia/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    # path("<str:name>", views.wiki, name="wiki")
+    path("wiki/<str:name>/", views.wiki, name="wiki"),
+    path("search/", views.search, name="search"),
+    path("create/", views.create, name="create"),
+    path("rand/", views.rand, name="rand")
 ]
--- Week3/Project1/wiki/encyclopedia/views.py
+++ Week3/Project1/wiki/encyclopedia/views.py
@@ -1,22 +1,68 @@
+from django import forms
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+
+import random
+from markdown2 import Markdown
 
 from . import util
 
+class NewForm(forms.Form):
+    name = forms.CharField(label="Search Encyclopedia")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-def title(request, name):
-    return render(request, "wiki/title.html", {
-        "title": name.capitalize(),
-        "entry": util.get_entry(name)
-    })
+def wiki(request, name):
+    if util.get_entry(name):
+        entry = util.get_entry(name)
+        markdowner = Markdown()
+        return render(request, "encyclopedia/wiki.html", {
+            "title": name,
+            "entry": markdowner.convert("%s" % entry)
+        })
+    else:
+        return render(request, "encyclopedia/error.html")
+   
+
+def search(request):
+    if request.method == "POST":
+        form = request.POST
+        name = form['q']
+        if util.get_entry(name):
+            return HttpResponseRedirect('/wiki/%s/' % name)
+            # how to deal with capitalization and stuff?
+        else:
+            return render(request, "encyclopedia/search.html", {
+                "entries": util.list_entries(),
+                "substr": name
+            })
+
+def create(request):
+    if request.method == "POST":
+        form = request.POST
+        title = form['title']
+        content = form['content']
+        util.save_entry(title, content)
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
+# probably have to look over this again. the notpython entry markdown is all screwed up.
+
+    return render(request, "encyclopedia/create.html")
 
 
+# def edit(request):
+#   if request.method == "POST":
+#       return render(request, "encyclopedia/wiki.html", {
+                # TODO
+#       })
 
-# screwed something up with naming stuff wiki. 
-# prolly have to rewatch some of hte lecture
-# do 127.0.0.1:8000 to visit the index page, do /wiki/TITLE to get to the other page. Is it an app?
-# why is the encyclopedia app index page viewable without any /encyclopedia? it was like that for other  apps
+
+def rand(request):
+    entries = util.list_entries()
+    name = random.choice(entries)
+    return HttpResponseRedirect('/wiki/%s/' % name)
