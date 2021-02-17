9147d53a1be03dfeec14ec26849fdbeeffdf62f6
bobycat22 <anthonyznj@gmail.com>
2021-02-16 23:07:35 -0500

    fixed javascript issue

--- Week7/Project4/project4/network/static/network/posts.js
+++ Week7/Project4/project4/network/static/network/posts.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    document.getElementByClassName("posts-view").addEventListener('click', function(event) {
+    document.getElementsByClassName("#posts-view").addEventListener('click', function(event) {
+        // problem here
         const element = event.target;
+        console.log(element);
         if (element.classList.containts("edit")) {
             // problem here -- would probably just return the first one
             const parent = element.parentNode;
@@ -44,48 +46,48 @@ function load_page(kind) {
 }
 
 
-function load_posts(post) {
+// function load_posts(post) {
 
-    document.querySelector('posts-view').innerHTML = '';
-    const post = document.createElement('div');
-    post.className = "view-post";
-    const user = document.createElement('h6');
-    user.innerHTML = `<b> <a href='/profile/{% post.user.id %}/'> ${post.user} </a> </b>`;
-    const content = document.createElement('p');
-    content.innerHTML = `${post.content}`;
-    const timestamp = document.createElement('p');
-    timestamp.innerHTML = `${post.datetime}`;
-    const likes = document.createElement('p');
-    likes.innerHTML = `${post.likes}`;
-    likes.id = 'likes';
-    let liked = false;
-    document.addEventListener('click', function(event) {
-        const element = event.target;
-        if (element.id === 'likes' && liked === false) {
-            fetch(`/posts/${post.id}`, {
-                method: 'PUT',
-                body: JSON.stringify({
-                    likes: (likes+1)
-                })
-            })
-            liked = true;
-            // location.reload()?
-        } else if (element.id === 'likes' && liked === true) {
-            fetch(`/posts/${post.id}`, {
-                method: 'PUT',
-                body: JSON.stringify({
-                    likes: (likes-1)
-                })
-            })
-            liked = false;            
-        }
-    })
+//     document.querySelector('posts-view').innerHTML = '';
+//     const post = document.createElement('div');
+//     post.className = "view-post";
+//     const user = document.createElement('h6');
+//     user.innerHTML = `<b> <a href='/profile/{% post.user.id %}/'> ${post.user} </a> </b>`;
+//     const content = document.createElement('p');
+//     content.innerHTML = `${post.content}`;
+//     const timestamp = document.createElement('p');
+//     timestamp.innerHTML = `${post.datetime}`;
+//     const likes = document.createElement('p');
+//     likes.innerHTML = `${post.likes}`;
+//     likes.id = 'likes';
+//     let liked = false;
+//     document.addEventListener('click', function(event) {
+//         const element = event.target;
+//         if (element.id === 'likes' && liked === false) {
+//             fetch(`/posts/${post.id}`, {
+//                 method: 'PUT',
+//                 body: JSON.stringify({
+//                     likes: (likes+1)
+//                 })
+//             })
+//             liked = true;
+//             // location.reload()?
+//         } else if (element.id === 'likes' && liked === true) {
+//             fetch(`/posts/${post.id}`, {
+//                 method: 'PUT',
+//                 body: JSON.stringify({
+//                     likes: (likes-1)
+//                 })
+//             })
+//             liked = false;            
+//         }
+//     })
 
-    post.append(user, content, timestamp, likes)
-    document.querySelector('#posts-view').append(post)
+//     post.append(user, content, timestamp, likes)
+//     document.querySelector('#posts-view').append(post)
 
 
-}
+// }
 
 
 
